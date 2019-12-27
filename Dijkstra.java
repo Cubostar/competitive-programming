f609bfa6340b0e98fbbd56174cc8eabb675df824
Cubostar <cubostar@gmail.com>
2019-12-27 11:42:56 -0800

    Polish Dijkstra (Idr what I did but still works)

--- Dijkstra.java
+++ Dijkstra.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 class Dijikstra {
 
 	static class Edge implements Comparable<Edge> {
-		public int node1;
-		public int node2;
+		public int vertex1;
+		public int vertex2;
 		public int length;
 
-		public Edge(int node1, int node2, int length){
-			this.node1 = node1;
-			this.node2 = node2;
+		public Edge(int vertex1, int vertex2, int length){
+			this.vertex1 = vertex1;
+			this.vertex2 = vertex2;
 			this.length = length;
 		}
 
@@ -20,12 +20,12 @@ class Dijikstra {
 		}
 
 		public String toString(){
-			return(node1 + ", " + node2 + ", " + length);
+			return(vertex1 + ", " + vertex2 + ", " + length);
 		}
 	}
 
-    // Finds shortest distance to each node from one node
-    static int[] dijkstra(int[][] graph, int node1){
+    // Finds shortest distance from one node to each node
+    static int[] dijkstra(int[][] graph, int vertex1){
 		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
 		int[] dijkstra = new int[graph.length];
 		for (int i = 0; i < graph.length; i++){
@@ -33,60 +33,66 @@ class Dijikstra {
 		}
         
         for (int i = 0; i < graph.length; i++){
-			if (graph[node1][i] != -1){
-				pq.add(new Edge(1, i, graph[node1][i]));
+			if (graph[vertex1][i] != -1){
+				pq.add(new Edge(1, i, graph[vertex1][i]));
 			}
 		}
 		
 		while (!pq.isEmpty()){
 			Edge e = pq.poll();
-			System.out.println(e);
-			if (dijkstra[e.node2] != -1){
+
+			// For debugging:
+			// System.out.println(e);
+			
+			if (dijkstra[e.vertex2] != -1){
 				;
 			} else {
-				dijkstra[e.node2] = e.length;
+				dijkstra[e.vertex2] = e.length;
 				for (int i = 0; i < graph.length; i++){
-					if (graph[e.node2][i] != -1){
-						pq.add(new Edge(e.node2, i, graph[e.node2][i] + e.length));
+					if (graph[e.vertex2][i] != -1){
+						pq.add(new Edge(e.vertex2, i, graph[e.vertex2][i] + e.length));
 					}
 				}
 			}
 		}
 
 		return(dijkstra);
-    }
-    // Finds shortest distance between two nodes
-    static int dijkstra(int[][] graph, int node1, int node2){
-        int[] dijkstra = dijkstra(graph, node1);
-        return(dijkstra[node2]);
+	}
+
+    // Finds shortest distance between two vertices
+    static int dijkstra(int[][] graph, int vertex1, int vertex2){
+        int[] dijkstra = dijkstra(graph, vertex1);
+        return(dijkstra[vertex2]);
     }
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int nodes = sc.nextInt();
+		int vertices = sc.nextInt();
 		int edges = sc.nextInt();
-        int[][] graph = new int[nodes + 1][nodes + 1];
+        int[][] graph = new int[vertices + 1][vertices + 1];
         
-		// Adjacency matrix version (nodes start counting at 1, not zero)
-		for (int i = 0; i < nodes + 1; i++) { // Initialize matrix to have -1 in each entry
-			for (int j = 0; j < nodes + 1; j++) {
+		// Adjacency matrix version (vertices start counting at 1, not zero)
+		for (int i = 0; i < vertices + 1; i++) { // Initialize matrix to have -1 in each entry
+			for (int j = 0; j < vertices + 1; j++) {
 				if (i == j){
 					graph[i][j] = 0;
 				}
 				graph[i][j] = -1;
 			}
 		}
-		for (int lengths = 0; lengths < edges; lengths++) { //Takes inputs and abstracts them
-			int node1 = sc.nextInt();
-			int node2 = sc.nextInt();
+		for (int lengths = 0; lengths < edges; lengths++) { //Takes inputs and adds them to matrix
+			int vertex1 = sc.nextInt();
+			int vertex2 = sc.nextInt();
 			int length = sc.nextInt();
+			
 			// If there's recurring paths, you may want to check if the preexisting entry is lower (but not -1)
-			graph[node1][node2] = length;
-			graph[node2][node1] = length;
+			graph[vertex1][vertex2] = length;
+			graph[vertex2][vertex1] = length;
 		}
 
-		int node1 = sc.nextInt();
-		int node2 = sc.nextInt();
-		System.out.println(dijkstra(graph, node1, node2));
+		int vertex1 = sc.nextInt();
+		int vertex2 = sc.nextInt();
+		System.out.println(dijkstra(graph, vertex1, vertex2));
+
 		sc.close();
 	}
 }
