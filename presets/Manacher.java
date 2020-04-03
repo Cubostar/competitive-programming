523c57996d6b4b5aeb9454f58f6223238da3de4b
Cubostar <quinn.arbolante@gmail.com>
2020-04-03 13:21:49 -0700

    Drafted Manacher's algorithm

--- presets/Manacher.java
+++ presets/Manacher.java
@@ -1,5 +1,6 @@
 package presets;
 import java.util.Scanner;
+import java.util.Arrays;
 
 // O(n) time
 // Assumes the character '#' can not appear in the given String (uses it as the dummy character)
@@ -47,23 +48,58 @@ class Manacher {
     }
 
     static int manacher(String s){
-        String n = "";
-
-        if (s.length() % 2 == 0){
-            for (int i = 0; i < s.length(); i++){
-                n += "#" + s.charAt(i);
+        char[] n = new char[(s.length() * 2)];
+        Arrays.fill(n, '#');
+        for (int i = 0; i < n.length; i++){
+            if (i % 2 == 1){
+                n[i] = s.charAt(i);
             }
-            n += "#";
-        } else {
-            n = s;
         }
+
+        int[] lengths = new int[n.length];
+        Arrays.fill(lengths, 0);
+        int c = 2; // center of the palindrome with the largest r
+        int l = 2; // left bound of palindrome about c
+        int r = 2; // right bound of palindrome about c
+        int i = c; // current center being evaluated
+
+        while (r < n.length){
+            if (c - lengths[c - i] < l){
+                lengths[i] = r - i;
+            } else {
+                lengths[i] = lengths[c - i];
+            }
+
+            for (int j = lengths[i] + 1; i - j > 0 && i + j < n.length; j++){
+                 if (n[i - j] == n[i + j]){
+                     lengths[i]++;
+                     if (i + j > r){
+                         c = i;
+                         l = c - j;
+                         r = c + j;
+                     }
+                 }
+            }
+            i++;
+        }
+
+        for (int j = i + 1; j < n.length; j++){
+            lengths[i + j] = lengths[i - j];
+        }
+
+        int m = -1;
+        for (int j = 1; j < n.length; j++){
+            if (lengths[j] > m) m = lengths[j];
+        }
+
+        return m;
     }
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         String s = sc.next();
 
-        System.out.println(squared(s));
+        System.out.println(manacher(s));
 
         sc.close();
     }
