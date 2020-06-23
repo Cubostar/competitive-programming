9a49eb6f675b32eddba5d313b998a0bbb2779936
Cubostar <quinn.arbolante@gmail.com>
2020-06-23 15:20:43 -0700

    Edit Manacher.java, add Fun.java

---
+++ presets/Fun.java
@@ -0,0 +1,7 @@
+package presets;
+
+public class Fun {
+    public static void main (String[] args){
+        System.out.println("Fun");
+    }
+}
--- presets/Manacher.java
+++ presets/Manacher.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
 // Assumes the character '#' can not appear in the given String (uses it as the dummy character)
 class Manacher {
 
+    static void printArray(int[] arr){
+        System.out.println(" ");
+        for (int i = 0; i < arr.length; i++){
+            System.out.print(arr[i] + ", ");
+        }
+    }
+
     // Naive O(n^2) solution
     static int squared(String s){
         String n = "";
@@ -52,22 +59,26 @@ class Manacher {
         Arrays.fill(n, '#');
         for (int i = 0; i < n.length; i++){
             if (i % 2 == 1){
-                n[i] = s.charAt(i);
+                n[i] = s.charAt(i / 2);
             }
         }
 
-        int[] lengths = new int[n.length];
+        int[] lengths = new int[n.length]; // array that holds the radius of each center's palindrome length
         Arrays.fill(lengths, 0);
         int c = 2; // center of the palindrome with the largest r
         int l = 2; // left bound of palindrome about c
         int r = 2; // right bound of palindrome about c
         int i = c; // current center being evaluated
+        int mirror = 0; // mirror index of i
 
         while (r < n.length){
-            if (c - lengths[c - i] < l){
+            printArray(lengths);
+            mirror = c - (i - c);
+            System.out.println("c: " + c + ", r: " + r + ", l: " + l + ", i: "+ i + ", mirror: " + mirror);
+            if (c - lengths[mirror] < l){
                 lengths[i] = r - i;
             } else {
-                lengths[i] = lengths[c - i];
+                lengths[i] = lengths[mirror];
             }
 
             for (int j = lengths[i] + 1; i - j > 0 && i + j < n.length; j++){
@@ -86,12 +97,13 @@ class Manacher {
         for (int j = i + 1; j < n.length; j++){
             lengths[i + j] = lengths[i - j];
         }
-
+ 
         int m = -1;
         for (int j = 1; j < n.length; j++){
-            if (lengths[j] > m) m = lengths[j];
+            if (lengths[j] > m) m = j;
         }
 
+        m = (n[m] == '#') ? m : m + 1;
         return m;
     }
 
